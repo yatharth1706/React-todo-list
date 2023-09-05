8002f6e3729202911c725735a9aa2141e304c3ac
RoshanDasan <edroshan2001@gmail.com>
2023-09-05 12:20:46 +0530

    error corrected

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import EditTask from '../modals/EditTask';
-import swal from 'sweetalert';
+import React, { useState } from "react";
+import EditTask from "../modals/EditTask";
+import swal from "sweetalert";
 
 const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
   const [modal, setModal] = useState(false);
 
   const colors = [
     {
-      primaryColor: '#5D93E1',
-      secondaryColor: '#ECF3FC'
+      primaryColor: "#5D93E1",
+      secondaryColor: "#ECF3FC",
     },
     {
-      primaryColor: '#F9D288',
-      secondaryColor: '#FEFAF1'
+      primaryColor: "#F9D288",
+      secondaryColor: "#FEFAF1",
     },
     {
-      primaryColor: '#5DC250',
-      secondaryColor: '#F2FAF1'
+      primaryColor: "#5DC250",
+      secondaryColor: "#F2FAF1",
     },
     {
-      primaryColor: '#F48687',
-      secondaryColor: '#FDF1F1'
+      primaryColor: "#F48687",
+      secondaryColor: "#FDF1F1",
     },
     {
-      primaryColor: '#B964F7',
-      secondaryColor: '#F3F0FD'
-    }
+      primaryColor: "#B964F7",
+      secondaryColor: "#F3F0FD",
+    },
   ];
 
   const toggle = () => {
@@ -39,35 +39,47 @@ const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
   const handleDelete = () => {
     swal("Here's the title!", "...and here's the text!");
     deleteTask(index);
-
   };
 
   return (
     <div className="card-wrapper mr-5">
-      <div className="card-top" style={{ backgroundColor: colors[index % 5].primaryColor }}></div>
+      <div
+        className="card-top"
+        style={{ backgroundColor: colors[index % 5].primaryColor }}
+      ></div>
       <div className="task-holder">
         <span
           className="card-header"
-          style={{ backgroundColor: colors[index % 5].secondaryColor, borderRadius: '10px' }}
+          style={{
+            backgroundColor: colors[index % 5].secondaryColor,
+            borderRadius: "10px",
+          }}
         >
           {taskObj.Name}
         </span>
         <p className="mt-3">{taskObj.Description}</p>
 
-        <div style={{ position: 'absolute', right: '20px', bottom: '20px' }}>
+        <div style={{ position: "absolute", right: "20px", bottom: "20px" }}>
           <i
             className="far fa-edit mr-3"
-            style={{ color: colors[index % 5].primaryColor, cursor: 'pointer' }}
+            style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
             onClick={() => setModal(true)}
           ></i>
           <i
             className="fas fa-trash-alt"
-            style={{ color: colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {handleDelete}></i>
-                </div>
+            style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
+            onClick={handleDelete}
+          ></i>
         </div>
-        <EditTask modal = {modal} toggle = {toggle} updateTask = {updateTask} taskObj = {taskObj}/>
-        </div>
-    );
+      </div>
+      <EditTask
+        modal={modal}
+        toggle={toggle}
+        updateTask={updateTask}
+        taskObj={taskObj}
+      />
+    </div>
+  );
 };
 
 export default Card;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,86 +1,106 @@
-import React, {useEffect, useState} from 'react';
-import CreateTask from '../modals/CreateTask'
-import Card from './Card';
-import swal from 'sweetalert';
-
+import React, { useEffect, useState } from "react";
+import CreateTask from "../modals/CreateTask";
+import Card from "./Card";
+import swal from "sweetalert";
 
 const TodoList = () => {
-    const [modal, setModal] = useState(false);
-    const [taskList, setTaskList] = useState([])
-    
-    useEffect(() => {
-        let arr = localStorage.getItem("taskList")
-       
-        if(arr){
-            let obj = JSON.parse(arr)
-            setTaskList(obj)
-        }
-    }, [])
+  const [modal, setModal] = useState(false);
+  const [taskList, setTaskList] = useState([]);
 
+  useEffect(() => {
+    let arr = localStorage.getItem("taskList");
 
-    const deleteTask = (index) => {
-        swal({
-            title: "Are you sure?",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-              swal("Your Todo list has been deleted!", {
-                icon: "success",
-              }).then(()=>
-              {
-                let tempList = taskList
-                tempList.splice(index, 1)
-                localStorage.setItem("taskList", JSON.stringify(tempList))
-                setTaskList(tempList)
-                window.location.reload()
-              })
-            } else {
-              swal("canceled");
-            }
-          })
-       
+    if (arr) {
+      let obj = JSON.parse(arr);
+      setTaskList(obj);
     }
+  }, []);
 
-    const updateListArray = (obj, index) => {
-        let tempList = taskList
-        tempList[index] = obj
-        localStorage.setItem("taskList", JSON.stringify(tempList))
-        setTaskList(tempList)
-        window.location.reload()
-    }
-
-    const toggle = () => {
-        setModal(!modal);
-    }
-
-    const saveTask = (taskObj) => {
-        swal({
-            title: "List created",
-            icon: "success",
-            button: "Ok",
+  const deleteTask = (index) => {
+    swal({
+      title: "Are you sure?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        swal("Your Todo list has been deleted!", {
+          icon: "success",
+        }).then(() => {
+          let tempList = taskList;
+          tempList.splice(index, 1);
+          localStorage.setItem("taskList", JSON.stringify(tempList));
+          setTaskList(tempList);
+          window.location.reload();
         });
-        const tempList = [...taskList, taskObj];
-        localStorage.setItem("taskList", JSON.stringify(tempList));
-        setTaskList(tempList);
-        setModal(false);
+      } else {
+        swal("canceled");
+      }
+    });
+  };
+
+  const updateListArray = (obj, index) => {
+    let tempList = taskList;
+    tempList[index] = obj;
+    localStorage.setItem("taskList", JSON.stringify(tempList));
+    setTaskList(tempList);
+    window.location.reload();
+  };
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  const saveTask = (taskObj) => {
+    swal({
+      title: "List created",
+      icon: "success",
+      button: "Ok",
+    });
+    const tempList = [...taskList, taskObj];
+    localStorage.setItem("taskList", JSON.stringify(tempList));
+    setTaskList(tempList);
+    setModal(false);
+  };
+
+  const deleteCards = () => {
+    if (setTaskList.length) {
+      swal({
+        title: "List cleared",
+        icon: "error",
+        button: "Ok",
+      });
     }
 
+    setTaskList([]);
+    localStorage.clear();
+  };
 
-    return (
-        <>
-            <div className = "header text-center">
-                <h3>Todo List</h3>
-                <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)} >Create Task</button>
-            </div>
-            <div className = "task-container">
-            {taskList && taskList.map((obj , index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask} updateListArray = {updateListArray}/> )}
-            </div>
-            <CreateTask toggle = {toggle} modal = {modal} save = {saveTask}/>
-        </>
-    );
+  return (
+    <>
+      <div className="header text-center">
+        <h3>Todo List</h3>
+        <button className="btn btn-primary m-2" onClick={() => setModal(true)}>
+          Create Task
+        </button>
+        <button className="btn btn-danger m-2" onClick={deleteCards}>
+          Clear
+        </button>
+      </div>
+      <div className="task-container">
+        {taskList &&
+          taskList.map((obj, index) => (
+            <Card
+              taskObj={obj}
+              index={index}
+              deleteTask={deleteTask}
+              updateListArray={updateListArray}
+            />
+          ))}
+      </div>
+      <CreateTask toggle={toggle} modal={modal} save={saveTask} />
+    </>
+  );
 };
 
 export default TodoList;
